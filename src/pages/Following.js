c60ef0b0e2688c216ae68e4bb52e6a15862a92e4
Rohan Sasne <rohansasne30@gmail.com>
2023-02-16 00:43:54 +0530

    Updated file structure

--- src/pages/Following.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ListContentVideo from "~/features/videos/components/ListContentVideo";
-
-function Following() {
-  return <ListContentVideo type="following" />;
-}
-
-export default Following;
--- src/pages/Live.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Live() {
-  return <h1>Live</h1>;
-}
-
-export default Live;
